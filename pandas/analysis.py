96680c2a89f430b8653153008366df3ce13951e9
Suvradip <suvradip@yahoo.co.in>
2025-09-18 17:14:41 +0100

    changes

--- pandas/analysis.py
+++ pandas/analysis.py
@@ -2,7 +2,28 @@ import pandas as pd
 import os
 
 os.system("cls")
+print("----------------------------------------------------------")
 
-program_df = pd.read_csv("AJ21-D4 Launch 1 NS.csv",encoding='utf-16',delimiter='\t')
-#print(program_df.head(10))
-plans_df= program_df.loc[program_df["Type"]=="APQP",["Type","Id","Summary"]]
+
+def get_plan_id(row):
+    if row["Type"] == "Deliverable":
+        part_before_colon = row["Parent"].split(":")[0]
+        number = part_before_colon.strip("#")
+    else:
+        number = row["Id"]
+    return number
+
+
+program_df = pd.read_csv("AJ21-D4 Launch 1 NS.csv",
+                         encoding='utf-16', delimiter='\t')
+program_df["Extracted_Parent"] = program_df.apply(get_plan_id, axis=1)
+
+new_df = program_df[["Type", "Id", "Summary", "Extracted_Parent"]]
+new_df["Extracted_Parent"] = pd.to_numeric(new_df["Extracted_Parent"])
+
+grouped = new_df.groupby("Extracted_Parent")
+output_dir = "Grouped_By_Parent"
+
+for plan_id, items in grouped:
+    filename = f"{output_dir}/{plan_id}_group.csv"
+    items.to_csv(filename, index=False)
