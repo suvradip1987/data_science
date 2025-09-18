1a821e6e07a9151c194d1cf0f1f3ad9310690b1d
Suvradip Mondal <35128576+suvradip1987@users.noreply.github.com>
2025-09-18 17:17:20 +0100

    Update analysis.py

--- pandas/analysis.py
+++ pandas/analysis.py
@@ -2,18 +2,28 @@ import pandas as pd
 import os
 
 os.system("cls")
+print("----------------------------------------------------------")
+
 
 def get_plan_id(row):
-    
-    # Step 1: Split by colon to isolate the part before it
-    part_before_colon = row.split(":")[0]
-    
-    # Step 2: Remove the '#' and get the number
-    number = part_before_colon.strip("#")
+    if row["Type"] == "Deliverable":
+        part_before_colon = row["Parent"].split(":")[0]
+        number = part_before_colon.strip("#")
+    else:
+        number = row["Id"]
     return number
 
-program_df = pd.read_csv("AJ21-D4 Launch 1 NS.csv",encoding='utf-16',delimiter='\t')
-#print(program_df.head(10))
-plans_df= program_df.loc[program_df["Type"]=="APQP",["Type","Id","Summary"]]
-program_df["Extracted_Parent"]= program_df.loc[program_df["Type"]=="Deliverable","Parent"].apply(get_plan_id)
-print(program_df[["Type","Id","Extracted_Parent"]].head(50))
+
+program_df = pd.read_csv("AJ21-D4 Launch 1 NS.csv",
+                         encoding='utf-16', delimiter='\t')
+program_df["Extracted_Parent"] = program_df.apply(get_plan_id, axis=1)
+
+new_df = program_df[["Type", "Id", "Summary", "Extracted_Parent"]]
+new_df["Extracted_Parent"] = pd.to_numeric(new_df["Extracted_Parent"])
+
+grouped = new_df.groupby("Extracted_Parent")
+output_dir = "Grouped_By_Parent"
+
+for plan_id, items in grouped:
+    filename = f"{output_dir}/{plan_id}_group.csv"
+    items.to_csv(filename, index=False)
