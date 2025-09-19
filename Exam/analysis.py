a9353b22bcae0288c09a7587117ed639f2de959c
Suvradip <suvradip@yahoo.co.in>
2025-09-19 01:14:05 +0100

    exam analysis changes

--- Exam/analysis.py
+++ Exam/analysis.py
@@ -2,17 +2,22 @@ import pandas as pd
 import os
 
 os.system("cls")
+print("----------------------------------------------")
 
-food_delivery_df = pd.read_csv("food_dely.csv")
-filtered_df = food_delivery_df[["name", "establishment", "city", "locality",
-                                "cuisines", "average_cost_for_two", "price_range", "highlights", "aggregate_rating",
-                                "rating_text", "votes" ,"delivery"]]
-#print(filtered_df.head(5))
-filtered_df.info()
-print(filtered_df.isnull().sum())
-filtered_df["establishment"]= filtered_df["establishment"].fillna(filtered_df["establishment"].mode()[0]).astype("string")
-filtered_df["cuisines"]= filtered_df["cuisines"].fillna(filtered_df["cuisines"].ffill())
-filtered_df["highlights"]= filtered_df["highlights"].fillna(filtered_df["highlights"].ffill())
-#print(filtered_df.isnull().sum())
-print(filtered_df["average_cost_for_two"].describe())
-filtered_df.info()
+food_df = pd.read_csv("food_dely.csv")
+# print(filtered_df.head(5))
+print(food_df.isnull().sum())
+food_df["establishment"] = food_df["establishment"].fillna(
+    food_df["establishment"].mode()[0]).astype("string")
+food_df["cuisines"] = food_df["cuisines"].fillna(
+    food_df["cuisines"].ffill())
+food_df["highlights"] = food_df["highlights"].fillna(
+    food_df["highlights"].ffill())
+food_df["name"] = food_df["name"].astype("string")
+food_df["city"] = food_df["name"].astype("string")
+food_df['cuisines'] = food_df['cuisines'].apply(lambda x: [c.strip() for c in x.split(',')])
+food_df.rename(columns={'country_id': 'country'}, inplace=True)
+food_df['country']= food_df['country'].map({1:"India"}).astype("string")
+print(food_df.isnull().sum())
+# print(filtered_df.isnull().sum())
+print(food_df["average_cost_for_two"].describe())
